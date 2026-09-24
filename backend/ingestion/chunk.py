02f3661abbd17ede79c68f39d71fbfb872b0acdf
Thejas <thejas@webenza.com>
2026-09-24 12:16:18 +0530

    [2026-09-24 12:55] - chunking improvements

--- backend/ingestion/chunk.py
+++ backend/ingestion/chunk.py
@@ -1,6 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def split_docs(documents, chunk_size=1000, chunk_overlap=100):
+def split_docs(documents, chunk_size=1200, chunk_overlap=200):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size, 
         chunk_overlap=chunk_overlap,
